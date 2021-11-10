411e66fa825f4ab6655bf61ff08351985ab78cf8
Ben Woodard <ben.woodard@searchdiscovery.com>
2021-11-10 15:52:53 -0500

    add query params to audit logs function

--- R/cja_get_audit_logs.R
+++ R/cja_get_audit_logs.R
@@ -2,6 +2,17 @@
 #'
 #' This function will pull a list of company ids that you have access to.
 #'
+#' @param startDate Date is not required, but if you filter by date, both start & end date must be set.
+#' @param endDate Date is not required, but if you filter by date, both start & end date must be set.
+#' @param action The action you want to filter by.
+#' @param component The type of component you want to filter by.
+#' @param componentId The ID of the component.
+#' @param userType The type of user.
+#' @param userId The ID of the user.
+#' @param userEmail The email address of the user.
+#' @param description The log description you want to filter by.
+#' @param pageSize Number of results per page. If left null, the default size will be set to 100.
+#' @param pageNumber Page number (base 0 - first page is "0")
 #' @param client_id Set in environment args, or pass directly in the function
 #' @param client_secret Set in environment args, or pass directly in the function
 #' @param org_id Set in environment args or pass directly in the function
@@ -14,7 +25,18 @@
 #' @export
 #' @import assertthat httr dplyr
 #'
-cja_get_audit_logs <- function(client_id = Sys.getenv("CJA_CLIENT_ID"),
+cja_get_audit_logs <- function(startDate = NA,
+                               endDate = NA,
+                               action = NA,
+                               component = NA,
+                               componentId = NA,
+                               userType = NA,
+                               userId = NA,
+                               userEmail = NA,
+                               description = NA,
+                               pageSize = 100,
+                               pageNumber = 0,
+                               client_id = Sys.getenv("CJA_CLIENT_ID"),
                                client_secret = Sys.getenv("CJA_CLIENT_SECRET"),
                                org_id = Sys.getenv('CJA_ORGANIZATION_ID')) {
     assertthat::assert_that(
@@ -22,10 +44,31 @@ cja_get_audit_logs <- function(client_id = Sys.getenv("CJA_CLIENT_ID"),
         assertthat::is.string(client_secret),
         assertthat::is.string(org_id)
     )
+    if(pageSize > 1000){
+        stop("The argument `pageSize` can not be more than 1,000. Use `pageNumber` to get the next set of results if more than 1,000 results are needed.")
+    }
+    vars <- tibble::tibble(startDate,
+                           endDate,
+                           action,
+                           component,
+                           componentId,
+                           userType,
+                           userId,
+                           userEmail,
+                           description,
+                           pageSize,
+                           pageNumber)
+
+
+    #Turn the list into a string to create the query
+    prequery <- vars %>% purrr::discard(~all(is.na(.) | . ==""))
+    #remove the extra parts of the string and replace it with the query parameter breaks
+    query_param <-  paste(names(prequery), prequery, sep = '=', collapse = '&')
+
     req_path = 'auditlogs/api/v1/auditlogs'
 
-    request_url <- sprintf("https://cja.adobe.io/%s",
-                           req_path)
+    request_url <- sprintf("https://cja.adobe.io/%s?%s",
+                           req_path, query_param)
 
     token_config <- get_token_config(client_id = client_id, client_secret = client_secret)
 
@@ -43,7 +86,7 @@ cja_get_audit_logs <- function(client_id = Sys.getenv("CJA_CLIENT_ID"),
 
     res <- httr::content(req, as = "text",encoding = "UTF-8")
 
-    logs <- jsonlite::fromJSON(res)
+    logs <- jsonlite::fromJSON(res)$content
 
     logs
 }
